f58fcffe6ba5cd6ca3a53d0133ef9a40e6ad074c
ValentinChernyaev <sergeich1997@yandex.ru>
2016-09-27 11:57:52 +0300

    Add files via upload

--- task_15.py
+++ task_15.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
 
-from pyrob.api import *
+import pyrob.core as rob
+import random
 
 
-@task
-def task_8_21():
-    pass
+class Task:
+    CHECKS = 4
+
+    def load_level(self, n):
+        rob.set_field_size(10, 10)
+
+        cases = {
+            0: ((0, 0), (9, 9)),
+            1: ((0, 9), (9, 0)),
+            2: ((9, 0), (0, 9)),
+            3: ((9, 9), (0, 0)),
+        }
+
+        pos, cpos = cases[n]
+
+        rob.set_parking_cell(*cpos)
+
+        rob.goto(*pos)
 
 
-if __name__ == '__main__':
-    run_tasks()
+    def check_solution(self):
+
+        return rob.is_parking_point()
