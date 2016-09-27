9887fa41dcf610df7dbd7a106c57238e1c4975ee
ValentinChernyaev <sergeich1997@yandex.ru>
2016-09-27 12:09:15 +0300

    Add files via upload

--- task_22.py
+++ task_22.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python3
 
-from pyrob.api import *
+import pyrob.core as rob
+import random
+from pyrob.tasks import check_filled_cells, find_cells_to_be_filled
 
 
-@task
-def task_5_10():
-    pass
+class Task:
+    CHECKS = 5
 
+    def load_level(self, n):
+        if n != self.CHECKS-1:
+            m = random.randint(5, 10)
+            n = random.randint(5, 10)
+        else:
+            m = 1
+            n = 1
 
-if __name__ == '__main__':
-    run_tasks()
+        rob.set_field_size(m, n)
+
+        for i in range(m):
+            for j in range(n):
+                rob.set_cell_type(i, j, rob.CELL_TO_BE_FILLED)
+
+        self.cells_to_fill = find_cells_to_be_filled()
+
+        rob.set_parking_cell(m-1, 0)
+
+        rob.goto(0, 0)
+
+    def check_solution(self):
+
+        return check_filled_cells(self.cells_to_fill) and rob.is_parking_point()
